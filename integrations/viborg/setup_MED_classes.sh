144dea00ed3e24e6a713b57d54e052f7e3468a2c
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2020-10-19 11:18:53 +0200

    Create seperate scripts for correct setup and fix for wrong setup in viborg_test

--- integrations/viborg/setup_MED_classes.sh
+++ integrations/viborg/setup_MED_classes.sh
@@ -4,6 +4,7 @@
 MOX_URL="${MOX_URL:-http://localhost:8080}"
 
 CLI="venv/bin/python os2mo_data_import/med/mox_util.py cli --mox-base ${MOX_URL}"
+
 # Organisation Unit Types
 #------------------------
 ${CLI} ensure-class-exists --bvn "HO-MED" --title "Hoved-MED" --facet-bvn "org_unit_type"
@@ -26,10 +27,4 @@ ${CLI} ensure-class-exists --bvn "MED-Medlem-suppleant" --title "MED-Medlem-supp
 #------------------------
 ${CLI} ensure-class-exists --bvn "MED-enhed" --title "MED-enhed" --facet-bvn "org_unit_level"
 
-##Checks - only to test out new feature "ensure-class-value":
-${CLI} ensure-class-value --bvn "HO-MED" --variable titel --new_value "Hoved-MED"
-${CLI} ensure-class-value --bvn "Hoved-MED"  --variable brugervendtnoegle --new_value "HO-MED"
-${CLI} ensure-class-value --bvn "FÆ-MED"  --variable titel --new_value "Fælles-MED2"
-${CLI} ensure-class-value --bvn "FÆ-MED"  --variable titel --new_value "Fælles-MED"
-${CLI} ensure-class-value --bvn "FÆ-MED"  --variable titel --new_value "Fælles-MED"
 
