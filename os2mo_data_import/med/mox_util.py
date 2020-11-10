64cd41a224f3233f775a7465fa76459990087dfc
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2020-11-10 11:32:18 +0100

    Add possibility to add owner

--- os2mo_data_import/med/mox_util.py
+++ os2mo_data_import/med/mox_util.py
@@ -249,8 +249,24 @@ async def ensure_class_value(
             return tuple(values), any(changed)
         else:
             return o, False
+    if variable == 'ejer':
+        try:
+            old_owner = klasse.get('relationer').get('ejer')[0].get('uuid')
+            changed = False if old_owner == new_value else True 
+        except:
+            changed = True
 
-    klasse, changed = check_value(variable, new_value, klasse)
+        if changed:
+            klasse['relationer']['ejer'] = [
+                {
+                    "uuid": new_value,
+                    "virkning": virkning,
+                    "objekttype": "OrganisationEnhed",
+                }
+            ]
+
+    else:
+        klasse, changed = check_value(variable, new_value, klasse)
     # Print for dry run
     if dry_run:
         mox_helper.validate_klassifikation_klasse(klasse)
