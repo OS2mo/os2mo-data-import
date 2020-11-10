2fe5eddb21839e76b68503f1ca35afe266d62a84
Benjamin Lyng Jørgensen <blj@magenta-aps.dk>
2020-11-10 12:48:38 +0100

    Add comments and use bvn's in stead of names

--- os2mo_data_import/med/Frederikshavn_set_owner.sh
+++ os2mo_data_import/med/Frederikshavn_set_owner.sh
@@ -5,27 +5,37 @@ MOX_URL="${MOX_URL:-http://localhost:8080}"
 
 CLI="venv/bin/python os2mo_data_import/med/mox_util.py cli --mox-base ${MOX_URL}"
 
-#Find UUID'er med: curl localhost:5001/service/o/a32e0b8d-4565-4280-8288-212b76cfac03/children | jq .
+UUID=$(curl --silent -H 'SESSION: '${SAML_TOKEN} localhost:5000/service/o/ | jq -r .[0].uuid)
+
+# Find UUID'er med: curl localhost:5001/service/o/${UUID}/children | jq .
+# og indsæt dem her:
 MED="96d2125e-7f5d-454a-a564-ce8ccb0b2d95"
 MAIN="7ddf4346-ce24-6ba5-7620-a1e7162fda68"
 
-UUID=$(curl --silent localhost:5001/service/o/ | jq -r .[0].uuid)
-CLASSES=$(curl localhost:5001/service/o/${UUID}/f/association_type/)
+# Find alle klasser med facetten association_type
+CLASSES=$(curl -H 'SESSION: '${SAML_TOKEN} localhost:5000/service/o/${UUID}/f/association_type/)
+
 TOTAL_CLASSES=$(echo $CLASSES | jq -r  .data.total)
 echo "Found ${TOTAL_CLASSES} association_types"
 
+# For disse klasser skal ejerskabet være MED organisationen:
 LIST='"AMR" "AMR, næstformand" "FTR" "FTR, næstformand" "LR" "LR, formand" "Medarb.rep, næstformand" "Stedfortræder" "TR" "TR, næstformand"'
 
+# Loop over klasserne
 echo $CLASSES | jq -c .data.items[] | while read line; do
     NAME=$(echo $line | jq .name)
     BVN=$(echo $line | jq .user_key)
+    # Fjern "
     NAME=${NAME//\"/}
-    
-    if [[ -z $(echo $LIST | grep -w "$NAME") ]]; then
+    BVN=${BVN//\"/}
+
+    # Tjek om navnet er i listen og sæt i så fald ejeren til MED-organisationen
+    if [[ -n $(echo $LIST | grep -w "$NAME") ]]; then
         OWNER=$MED
     else
         OWNER=$MAIN
     fi
     echo $NAME : $OWNER
+        
     ${CLI} ensure-class-value --bvn "${BVN}" --variable ejer --new_value "$OWNER" 
 done
