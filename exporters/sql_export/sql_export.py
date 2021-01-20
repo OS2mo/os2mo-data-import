4e77e227fcadc180f003560e11364ddc77771f65
Christoffer Moesgaard <christoffer@magenta.dk>
2021-01-20 11:30:57 +0100

    [#39316] Add address type user_key to LCDB

--- exporters/sql_export/sql_export.py
+++ exporters/sql_export/sql_export.py
@@ -266,6 +266,7 @@ class SqlExport(object):
                     værdi=address_info['value'],
                     dar_uuid=address_info['dar_uuid'],
                     adressetype_uuid=address_info['adresse_type'],
+                    adressetype_bvn=address_info['user_key'],
                     adressetype_scope=address_info['scope'],
                     adressetype_titel=self.lc.classes[
                         address_info['adresse_type']
